4973b77ad400c99da07a77721ff1df8336af75ba
m.ract <m.ract@groupeonepoint.com>
2023-11-07 16:47:12 +0100

    ajout partners de onepoint

--- bots/Neo_Breaker/bot_neo_breaker.py
+++ bots/Neo_Breaker/bot_neo_breaker.py
@@ -2,18 +2,11 @@ from selenium import webdriver as wd
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from data_partners import personnes
 import random as r
 import time as t
 
 nb_requêtes = 10
-names_team = ["Nicolas Gaudemet",
-              "Mathieu Ract",
-              "Thomas Cosyn",
-              "Alexander Morakhovski",
-              "Servane Gouillou",
-              "Maurits Reijnen",
-              "Stephan Adjarian",
-              "Amine Cherif Haouat"]
 
 opts = wd.FirefoxOptions()
 opts.set_preference("dom.popup_maximum", nb_requêtes)
@@ -33,29 +26,29 @@ for i in range(nb_requêtes):
     onglets = driver.window_handles
     driver.switch_to.window(onglets[i])
 
-    BOUTON_APPROFONDI = WebDriverWait(driver, 5).until(
+    BOUTON_APPROFONDI = WebDriverWait(driver, 60).until(
         EC.visibility_of_element_located((
             By.XPATH, XPATH_BOUTON_APPROFONDI)))
 
-    BOUTON_RAPIDE = WebDriverWait(driver, 5).until(
+    BOUTON_RAPIDE = WebDriverWait(driver, 60).until(
         EC.visibility_of_element_located((
             By.XPATH, XPATH_BOUTON_RAPIDE)))
 
-    ZONE_TEXTE = WebDriverWait(driver, 5).until(
+    ZONE_TEXTE = WebDriverWait(driver, 60).until(
         EC.visibility_of_element_located((
             By.XPATH, XPATH_ZONE_TEXTE)))
 
-    BOUTON_QUESTION = WebDriverWait(driver, 5).until(
+    BOUTON_QUESTION = WebDriverWait(driver, 60).until(
         EC.visibility_of_element_located((
             By.XPATH, XPATH_BOUTON_QUESTION)))
 
-    BOUTON_EFFACER = WebDriverWait(driver, 5).until(
+    BOUTON_EFFACER = WebDriverWait(driver, 60).until(
         EC.visibility_of_element_located((
             By.XPATH, XPATH_BOUTON_EFFACER)))
 
     BOUTON_APPROFONDI.click()
 
-    name = r.choice(names_team)
+    name = r.choice(personnes)
     question = f"Qui est {name} ?"
 
     print(f"Question {i+1}/{nb_requêtes} : {question}")
